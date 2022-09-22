291c827a06631f14af1d2cc17cbd8e7e518e394d
Samuel <samuel@families.dk>
2022-09-22 09:18:00 +0200

    fibonacci

--- 5_Assignment/Program.cs
+++ 5_Assignment/Program.cs
@@ -1,21 +1,26 @@
-﻿//Un-comment below code and fix the issues
-//Push to github
-/*
-int RecursiveFibonacci(int first, int second, int count)
+﻿int fibNum1 = 0;
+int fibNum2 = 1;
+int fibNum3,i;
+int maksfib = 41;
+
+Console.WriteLine("hello now i will show you some magic");
+Console.WriteLine("are you ready?");
+Console.ReadLine();
+Console.WriteLine("it did not matter what you said");
+Console.WriteLine("we have to numbers in the start");
+Thread.Sleep(2000);
+Console.WriteLine("0:" + fibNum1);
+Console.WriteLine("1:" + fibNum2);
+Thread.Sleep(2000);
+
+for(i=2;i<maksfib;++i)
 {
-    count--;
-    
-    
-    if(count == 0) {
-    
-    }else{
-        return RecursiveFibonacci(?, ?, ?);
-    }
+Console.Write(i + ":");
+fibNum3 = fibNum1 + fibNum2;
+Console.WriteLine( + fibNum3);
+fibNum1 = fibNum2;
+fibNum2 = fibNum3;
+Thread.Sleep(500);
 }
-
-int lastFibo = RecursiveFibonacci(1,1,5);
-Console.WriteLine("Last fibo number was " + lastFibo);
-Console.ReadLine(); 
-*/
-
-
+Console.WriteLine("now that was a total of 40 numbers of fibonacci");
+Console.ReadKey();
